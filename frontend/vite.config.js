50b124bf84a554e5626beb1633b5a1b21b69393d
ADAKULAPALLYPAVAN <116061497+adakulapallypavan@users.noreply.github.com>
2024-10-12 22:53:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,  // Change the frontend port to 5173 or another available port
     proxy: {
       "/api/": "http://localhost:3000",
       "/uploads/": "http://localhost:3000",
